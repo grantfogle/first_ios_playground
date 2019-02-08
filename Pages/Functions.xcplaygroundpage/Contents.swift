52cacceb9813fe74f75a6ed049dc5273ff748de5
grantfogle <grantjfogle@gmail.com>
2019-02-08 12:51:40 -0700

    learning functions

---
+++ Pages/Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+func area(length: Int, width: Int) -> Int {
+    let areaOfRoom = length * width
+    return areaOfRoom
+}
+
+area(length: 10, width: 20)
+area(length: 10, width: 8)
+
+//func someFunction() {}
+//func someFunction() -> () {}
+//func someFunction() -> Void {}
+
+func someFunction() -> Void {
+    
+}
