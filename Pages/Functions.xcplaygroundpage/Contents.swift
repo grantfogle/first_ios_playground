4aff1ac61ec06b57f62aa4c3146e4c84a68e73fd
grantfogle <grantjfogle@gmail.com>
2019-02-13 14:05:25 -0700

    swift scope and functions

--- Pages/Functions.xcplaygroundpage/Contents.swift
+++ Pages/Functions.xcplaygroundpage/Contents.swift
@@ -22,3 +22,36 @@ func remove(havingValue value: String) {
 }
 
 remove(havingValue: "A")
+
+func carpetCostHaving(length: Int, width: Int, carpetColor color:String = "tan") ->
+    (price: Int, carpetColor: String){
+    let areaOfRoom = area(length: length, width: width)
+    
+    var price = 0
+    
+    switch color {
+    case "gray": price = areaOfRoom * 1
+    case "tan": price = areaOfRoom * 2
+    case "blue": price = areaOfRoom * 4
+    default: price = 0
+    }
+    
+    return (price, color)
+}
+
+let result = carpetCostHaving(length: 10, width: 20)
+result.carpetColor
+result.price
+
+// scope
+
+func arrayModifier(array: [Int]){
+    var arrayOfInts = array
+    arrayOfInts.append(5)
+    
+    var secondArray = arrayOfInts
+}
+
+var arrayOfInts = [1,2,3,4]
+arrayModifier(array: arrayOfInts)
+arrayOfInts
