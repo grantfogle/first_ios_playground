28bea3488a974c7466b6d09d1594096db77211f1
grantfogle <grantjfogle@gmail.com>
2019-02-13 14:36:57 -0700

    learning about structs/objects

--- Pages/Object Oriented Swift.xcplaygroundpage/Contents.swift
+++ Pages/Object Oriented Swift.xcplaygroundpage/Contents.swift
@@ -1,7 +1,16 @@
-//: [Previous](@previous)
+let x1 = 0
+let y1 = 0
 
-import Foundation
+let coordinate1: (x: Int, y: Int) = (0,0)
+coordinate1.x
 
-var str = "Hello, playground"
+// struct is a flexible data type
+//user UpperCamelCase to start
+struct Point {
+    let x: Int
+    let y: Int
+}
 
-//: [Next](@next)
+//instance of a struct
+let coordinatePoint = Point(x: 0 , y: 0)
+coordinatePoint.x
