4b901a5e3fea1be72a636d12e8ca129333f96ba2
grantfogle <grantjfogle@gmail.com>
2019-02-13 18:42:17 -0700

    completed classes/structs section

--- Pages/ValueVsReference.xcplaygroundpage/Contents.swift
+++ Pages/ValueVsReference.xcplaygroundpage/Contents.swift
@@ -1,7 +1,31 @@
-//: [Previous](@previous)
+struct User {
+    var fullName: String
+    var email: String
+    var age: Int
+}
 
-import Foundation
+var someUser = User(fullName: "Grant fogle", email: "grantjfogle@gmail.com", age: 27)
 
-var str = "Hello, playground"
+var anotherUser = someUser
 
-//: [Next](@next)
+anotherUser.email = "granthockeyplayer@gmail.com"
+someUser.email
+anotherUser.email
+
+class Person {
+    var fullName: String
+    var email: String
+    var age: Int
+    
+    init(name: String, email: String, age: Int){
+        self.fullName = name
+        self.email = email
+        self.age = age
+    }
+}
+var somePerson = Person(name: "Brian Bathers", email: "oldguy@aol.com", age: 80)
+
+var anotherPerson = somePerson
+
+somePerson.email = "adiffemail@email.com"
+anotherPerson.email
